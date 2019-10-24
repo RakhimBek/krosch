dddf679d54960ecb3121e243edae2c73759acf38
idony <aptemhh@mail.ru>
2019-10-24 23:44:33 +0600

    client

--- scoket_client.py
+++ scoket_client.py
@@ -6,7 +6,7 @@ import asyncio
 import random
 import websockets
 
-current_state = [0,'']
+current_state = [0,'',False, 0]
 
 def start():
     return '''{ "team": "krosch"}'''
@@ -18,56 +18,60 @@ def run():
     async def hello(current_state):
         uri = "ws://localhost:8765"
         async with websockets.connect(uri) as websocket:
-            position = current_state[0]
 
-            if position == -1:
+            if current_state[2] == True:
                 await websocket.send(current_state[1])
-                position += 1
+                print(f"> {current_state[1]}")
+                current_state[2] = False
 
             name = start()
-            if position == 0 :
+            if current_state[0] == 0 :
                 await websocket.send(name)
                 print(f"> {name}")
                 current_state[1] = 'sad'
-                position += 1
+                current_state[0] += 1
 
-            if position == 1:
+            if current_state[0] == 1:
                 greeting = await websocket.recv()
                 print(f"< {greeting}")
-                position += 1
+                current_state[0] += 1
 
-            if position == 2:
+            if current_state[0] == 2:
                 routes = await websocket.recv()
                 print(f"< {routes}")
-                position += 1
+                current_state[0] += 1
 
 
             while True:
-                point = random.randint(0,9)
-                if position == 3:
-                    goto = sendGoto(point,"btr")
+                if current_state[0] == 3:
+                    current_state[3] = random.randint(0, 9)
+                    goto = sendGoto(current_state[3],"btr")
                     print(f"> {goto}")
                     await websocket.send(goto)
-                    position += 1
+                    current_state[0] += 1
 
-                if position == 4:
-                    if point == 1:
+                if current_state[0] == 4:
+                    if current_state[3] == 1:
                         teamsum = await  websocket.recv()
                         print(f"< {teamsum}")
-                    position += 1
+                    current_state[0] += 1
 
-                if position == 5:
+                if current_state[0] == 5:
                     points = await  websocket.recv()
                     print(f"< {points}")
-                    position += 1
+                    current_state[0] += 1
 
-                if position == 6:
+                if current_state[0] == 6:
                     traffic = await  websocket.recv()
                     print(f"< {traffic}")
-                    position = 3
+                    current_state[0] = 3
 
     asyncio.get_event_loop().run_until_complete(hello(current_state))
 
 if __name__ == '__main__':
     while True :
-        run()
+        try:
+            run()
+        except:
+            current_state[2] = True
+
