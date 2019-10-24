35ce6af808bd3cbe8ecddcf479b6df374a7455ea
Raimbek <rrakhimbekov@gmail.com>
2019-10-25 05:24:44 +0600

    socket_client: single machine

--- scoket_client.py
+++ scoket_client.py
@@ -9,7 +9,10 @@ import json
 
 # status / token / need recovery connect/ allcarsum
 from clusterize import get_regions
+from desicion import decision
 from networkx_util import routes_to_graph, points_map, traffic_to_graph
+
+CAR_VOLUME = 1_000_000;
 
 current_state = [0, '', False, 0]
 # save mode close soket  -  pos,car, было отправлено
@@ -30,7 +33,9 @@ context = {
     "cars": {},
     "last_traffic": {},
     "points": {},
-    "visited": set()
+    "visited": {0, 1},
+    "current_point": 0,
+    "current_car": ""
 }
 
 
@@ -65,9 +70,11 @@ def run():
                 print(f"< {greeting}")
                 request = json.loads(greeting)
                 current_state[1] = request["token"]
-                carsJ = request["cars"]
-                for car in carsJ:
-                    cars[car] = [0, 0]
+                cars_json = request["cars"]
+                for car_id in cars_json:
+                    context["cars"][car_id] = {"id": car_id, "volume": CAR_VOLUME}
+
+                context["current_car"] = request["cars"][0]
                 current_state[0] = 2
 
             # Server: { "routes":[{"a":0,"b":1,"time":1 ...
@@ -98,14 +105,26 @@ def run():
             while True:
                 # Client: { "goto": 2, "car": "sb0" }
                 if current_state[0] == 5:
+
                     if saveMode[2] == False:
-                        saveMode[0] = random.randint(0, 9)
-                        saveMode[1] = "btr"
+                        decision_result = decision(
+                            visited=context["visited"],
+                            routes=context["routes"],
+                            points=context["points"],
+                            regions=context["regions"],
+                            current_traffic=context["traffic"],
+                            current_point=context["current_point"],
+                            car_info=context["cars"][context["current_car"]],
+                            remained_distance=1000
+                        )
+                        saveMode[0] = decision_result["goto"]
+                        saveMode[1] = decision_result["car"]
                         saveMode[2] = True
+
                     goto = sendGoto(saveMode[0], saveMode[1])
-                    saveMode[2] = False
                     print(f"> {goto}")
                     await websocket.send(goto)
+                    saveMode[2] = False
                     current_state[0] = 6
 
                 # Server: {"teamsum":115906}
@@ -118,10 +137,12 @@ def run():
 
                 # Server: { "point": 1, "car": "sb0", "carsum": 0 }
                 if current_state[0] == 7:
-                    pointR = await  websocket.recv()
-                    print(f"< {pointR}")
-                    point_json = json.loads(pointR)
-                    cars[point_json["car"]] = [point_json["point"], point_json["carsum"]]
+                    point_data = await  websocket.recv()
+                    print(f"< {point_data}")
+                    point_json = json.loads(point_data)
+                    context["current_point"] = point_json["point"]
+                    context["cars"][point_json["car"]] = {"id": point_json["car"],
+                                                          "volume": CAR_VOLUME - point_json["carsum"]}
                     context["visited"].add(int(point_json["point"]))
                     current_state[0] = 8
 
@@ -138,8 +159,8 @@ def run():
 
 if __name__ == '__main__':
     while True:
-        #try:
+        try:
             run()
-        #except Exception as e:
-        #    print(str(e))
-        #    current_state[2] = True
+        except Exception as e:
+            print(str(e))
+            current_state[2] = True
