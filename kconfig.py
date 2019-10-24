6135733efb332e7bbf312d6794d6c413b892e3b3
YegorYeremenko <egor@yeremenko.com>
2019-10-24 10:19:44 +0300

    version 01

---
+++ kconfig.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#24.10.2019, created by Egor Eremenko
+# all Configuration
+
+
+server_ip_prod = '0.0.0.0'
+server_port_prod = '80'
+
+server_ip_test = '0.0.0.0'
+server_port_test = '80'
+
+if __name__ == '__main__':
+    pass
