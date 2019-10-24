82b256752b9065243c74a9b64f114df23f0f4150
idony <aptemhh@mail.ru>
2019-10-24 20:34:26 +0600

    client

--- scoket_client.py
+++ scoket_client.py
@@ -5,18 +5,43 @@
 import asyncio
 import websockets
 
+def start():
+    return '{ "team": "krosch"}'
+
+def sendGoto(goto, car):
+    return '{ "goto": '+str(goto)+', "car": "'+car+'" }'
 
 def run():
     async def hello():
         uri = "ws://localhost:8765"
         async with websockets.connect(uri) as websocket:
-            name = input("What's your name? ")
+            name = start()
 
             await websocket.send(name)
             print(f"> {name}")
 
             greeting = await websocket.recv()
             print(f"< {greeting}")
+
+            routes = await websocket.recv()
+            print(f"< {routes}")
+
+            points = await websocket.recv()
+            print(f"< {points}")
+
+            traffic = await websocket.recv()
+            print(f"< {traffic}")
+
+            for x in range(6):
+                goto = sendGoto(3,"btr")
+                print(f"> {goto}")
+                await websocket.send(goto)
+
+                points = await  websocket.recv()
+                print(f"< {points}")
+
+                traffic = await  websocket.recv()
+                print(f"< {traffic}")
 
     asyncio.get_event_loop().run_until_complete(hello())
 
